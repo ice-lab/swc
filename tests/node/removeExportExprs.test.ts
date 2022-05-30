79f64bec6f9d2fe10a503ca3fcf1c23967efb37f
fushen <fushen.jzw@alibaba-inc.com>
2022-05-30 19:30:18 +0800

    fix: monorepo workspace

--- tests/node/removeExportExprs.test.ts
+++ tests/node/removeExportExprs.test.ts
@@ -2,38 +2,41 @@ import { transformSync } from '../../node';
 
 describe('swc remove export expressions', () => {
   it('should preserve getConfig function', () => {
-    // const originalCode = `import fs from 'fs'
-    // import other from 'other'
+    const originalCode = `import fs from 'fs'
+    import other from 'other'
 
-    // const [a, b, ...rest] = fs.promises
-    // const [foo, bar] = other
+    const [a, b, ...rest] = fs.promises
+    const [foo, bar] = other
 
-    // export async function getData() {
-    //   a
-    //   b
-    //   rest
-    //   bar
-    // }
+    export async function getData() {
+      a
+      b
+      rest
+      bar
+    }
 
-    // export function getConfig() {
-    // }
+    export function getConfig() {
+    }
 
-    // export default function Home() {
-    //   return <div />
-    // }`;
+    export default function Home() {
+      return <div />
+    }`;
 
-    // const { code } = transformSync(originalCode, {
-    //   sourceMaps: false,
-    //   jsc: {
-    //     parser: {
-    //       syntax: 'ecmascript',
-    //       jsx: true,
-    //     },
-    //     target: 'es2016',
-    //   },
-    //   removeExportExprs: ['getData', 'default'],
-    // });
+    const { code } = transformSync(originalCode, {
+      sourceMaps: false,
+      jsc: {
+        parser: {
+          syntax: 'ecmascript',
+          jsx: true,
+        },
+        target: 'es2016',
+      },
+      removeExportExprs: ['getData', 'default'],
+    });
 
-    // expect(code).toEqual(``);
+    expect(code).toEqual(`import other from 'other';
+    const [foo] = other;
+    export function getConfig() {}
+    export default function() {};`);
   });
 });
