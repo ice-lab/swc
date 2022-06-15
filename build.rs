9bd8353b73ae8a96960987f7798a05fb9bbdd194
shuilan.cj <shuilan.cj@taobao.com>
2022-06-15 11:56:56 +0800

    chore: change test native

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ extern crate napi_build;
 
 fn main() {
     napi_build::setup();
-    println!("cargo:rustc-link-arg-tests=-Wl,--unresolved-symbols=ignore-all");
+    // println!("cargo:rustc-link-arg-tests=-Wl, --unresolved-symbols=ignore-all");
 }
